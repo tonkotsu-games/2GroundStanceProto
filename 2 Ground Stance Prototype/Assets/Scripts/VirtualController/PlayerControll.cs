30f2ecb01eb194d94cc53143f1f889f1bf8336f7
Niklas <niklashassmann@tonkotsu.games>
2019-09-24 12:02:46 +0200

    Camera Setup done

--- 2 Ground Stance Prototype/Assets/Scripts/VirtualController/PlayerControll.cs
+++ 2 Ground Stance Prototype/Assets/Scripts/VirtualController/PlayerControll.cs
@@ -29,7 +29,7 @@ public class PlayerControll : MonoBehaviour
     }
 
     private void Update()
-    {
+    {       
         if (inputPackage != null)
         {
             MovementCalculation();
